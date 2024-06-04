d9e96da6f12754b8964c266fb7c447e87a6ec457
Frank Ray <info@frankray.net>
2024-06-04 10:11:32 +0100

    Updated

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,13 @@
+#include "kernel.h"
+
+
+#define asmlinkage __attribute__((regparm(0)))
 
 #define VIDEO_ADDRESS 0xb8000
 #define WHITE_ON_BLUE  0x1f
 
-int kernel_main()
+
+asmlinkage int kernel_main()
 {
 	//Create a byte pointer to the start of video memory
     unsigned char* vidmem = (unsigned char*)VIDEO_ADDRESS;

0e7331fe0da70291529ca9dcd6050aa9d534cce5
Frank Ray <info@frankray.net>
2024-06-07 15:28:48 +0100

    Ascii art

--- src/kernel.c
+++ src/kernel.c
@@ -8,5 +8,19 @@ asmlinkage int kernel_main()
     
     printstring("Hello World!");
 
+
+    //Ascii art courtesy of https://www.asciiart.eu/text-to-ascii-art
+    //Doom font, width 80
+    /*
+    printline("                                                                         ");
+    printline("    _____           _                   _   _             _____ _____    ");
+    printline("   |_   _|         | |                 | | (_)           |  _  /  ___|   ");
+    printline("     | |  _ __  ___| |_ _ __ _   _  ___| |_ _  ___  _ __ | | | \\ `--.    ");
+    printline("     | | | '_ \\/ __| __| '__| | | |/ __| __| |/ _ \\| '_ \\| | | |`--. \\   ");
+    printline("    _| |_| | | \\__ \\ |_| |  | |_| | (__| |_| | (_) | | | \\ \\_/ /\\__/ /   ");
+    printline("   |_____|_| |_|___/\\__|_|   \\__,_|\\___|\\__|_|\\___/|_| |_|\\___/\\____/    ");
+    */
+
+
     return 0;
 }

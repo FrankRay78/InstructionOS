0e5755bd3a50fa95a7a0c3bb0f22e2b17db6d0d0
Frank Ray <info@frankray.net>
2024-06-07 19:52:36 +0100

    Updated tests, more passing

--- test/console_HelloWorld_tests.c
+++ test/console_HelloWorld_tests.c
@@ -1,13 +1,57 @@
 #include "console_HelloWorld_tests.h"
 
-MunitResult console_should_write_HelloWorld_test(const MunitParameter params[], void* user_data_or_fixture)
+MunitResult console_should_write_hello_world_with_print_statement_test(const MunitParameter params[], void* user_data_or_fixture)
 {
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_write_hello_world_with_print_statements_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_write_hello_CRLF_world_with_print_statement_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_write_hello_CRLF_world_with_print_statements_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_write_hello_CRLF_world_with_printline_statements_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    // Given
+    unsigned char* framebuffer = (unsigned char*)malloc(80 * 25 * 2);
+    console_initialise(80, 25, 2, framebuffer, 0x00);
+
+    // When
+    console_printline("Hello");
+    console_printline("World");
+
+    // Then
+    munit_assert_char('H', ==, framebuffer[0]);
+    munit_assert_char('e', ==, framebuffer[2]);
+    munit_assert_char('l', ==, framebuffer[4]);
+    munit_assert_char('l', ==, framebuffer[6]);
+    munit_assert_char('o', ==, framebuffer[8]);
+    munit_assert_char('W', ==, framebuffer[80 * 2 + 0]);
+    munit_assert_char('o', ==, framebuffer[80 * 2 + 2]);
+    munit_assert_char('r', ==, framebuffer[80 * 2 + 4]);
+    munit_assert_char('l', ==, framebuffer[80 * 2 + 6]);
+    munit_assert_char('d', ==, framebuffer[80 * 2 + 8]);
+
     return MUNIT_OK;
 }
 
 MunitTest console_HelloWorld_tests[] = 
 {
-    { "/console_should_write_HelloWorld_test", console_should_write_HelloWorld_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_write_hello_world_with_print_statement_test", console_should_write_hello_world_with_print_statement_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_write_hello_world_with_print_statements_test", console_should_write_hello_world_with_print_statements_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_write_hello_CRLF_world_with_print_statement_test", console_should_write_hello_CRLF_world_with_print_statement_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_write_hello_CRLF_world_with_print_statements_test", console_should_write_hello_CRLF_world_with_print_statements_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_write_hello_CRLF_world_with_printline_statements_test", console_should_write_hello_CRLF_world_with_printline_statements_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     /* Mark the end of the array with an empty entry */
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
--- test/console_ThreeByThree_tests.c
+++ test/console_ThreeByThree_tests.c
@@ -2,12 +2,42 @@
 
 MunitResult console_should_write_ABC_DEF_GHI_test(const MunitParameter params[], void* user_data_or_fixture)
 {
-    return MUNIT_OK;
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_clear_ABC_DEF_GHI_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_scroll_one_line_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_scroll_two_lines_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_scroll_three_lines_test(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
+}
+
+MunitResult console_should_scroll_and_blank_last_line(const MunitParameter params[], void* user_data_or_fixture)
+{
+    return MUNIT_FAIL;
 }
 
 MunitTest console_ThreeByThree_tests[] = 
 {
     { "/console_should_write_ABC_DEF_GHI_test", console_should_write_ABC_DEF_GHI_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_clear_ABC_DEF_GHI_test", console_should_clear_ABC_DEF_GHI_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_scroll_one_line_test", console_should_scroll_one_line_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_scroll_two_lines_test", console_should_scroll_two_lines_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_scroll_three_lines_test", console_should_scroll_three_lines_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { "/console_should_scroll_and_blank_last_line", console_should_scroll_and_blank_last_line, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     /* Mark the end of the array with an empty entry */
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };

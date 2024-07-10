3135d1dcced002b0d1a5bd3a64cbde5ddd8aa08d
Frank Ray <info@frankray.net>
2024-07-10 14:25:39 +0100

    Build script for the i686-elf cross compiler

--- i686-elf-gcc-build.sh
+++ i686-elf-gcc-build.sh
@@ -15,11 +15,9 @@
 set -e
 
 
-#echo "Press enter to continue"
-#read _
 
 echo "This script will build the i686-elf GCC cross compiler"
-echo "Do you wish to continue?"
+echo "Continue?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) break;;
@@ -27,4 +25,87 @@ select yn in "Yes" "No"; do
     esac
 done
 
-echo "install";
+
+
+echo "#"
+echo "# Build GCC and binutils from source"
+echo "#"
+
+
+echo "--- Installing packages ---";
+sudo apt install build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo
+
+
+mkdir -p ~/Source && pushd ~/Source
+
+
+echo "--- Downloading sources ---";
+wget https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.gz
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz
+
+
+echo "--- Extracting sources ---";
+tar -xf gcc-13.2.0.tar.gz
+tar -xf binutils-2.41.tar.xz
+
+
+echo "--- Compiling binutils ---";
+export PREFIX="$HOME/opt/gcc-13.2.0"
+mkdir -p build-binutils && pushd build-binutils
+../binutils-2.41/configure --prefix="$PREFIX" --disable-nls --disable-werror
+make -j$(nproc)
+make install
+popd
+
+
+echo "--- Downloading GCC prerequisites ---";
+pushd gcc-13.2.0
+./contrib/download_prerequisites
+popd
+
+
+echo "--- Compiling GCC ---";
+mkdir -p build-gcc && pushd build-gcc
+../gcc-13.2.0/configure --prefix="$PREFIX" --disable-nls --enable-languages=c,c++
+make -j$(nproc)
+make install
+popd
+
+
+
+echo "#"
+echo "# Build the i686-elf GCC cross compiler"
+echo "#"
+
+
+# nb. assumes we are following on from above
+export TARGET=i686-elf
+export PREFIX="$HOME/opt/cross"
+export PATH="$PREFIX/bin:$PATH"
+
+
+echo "--- Compiling binutils ---";
+rm -fdr build-binutils && mkdir -p build-binutils && pushd build-binutils
+../binutils-2.41/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make -j$(nproc)
+make install
+popd
+
+
+# The $PREFIX/bin directory _must_ be in the PATH
+which -- $TARGET-as
+
+
+echo "--- Compiling GCC ---";
+rm -fdr build-gcc && mkdir -p build-gcc && pushd build-gcc
+../gcc-13.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make -j$(nproc) all-gcc
+make -j$(nproc) all-target-libgcc
+make install-gcc
+make install-target-libgcc
+popd
+
+
+echo "--- Confirm installation of the compiler ---";
+$HOME/opt/cross/bin/$TARGET-gcc --version
+$HOME/opt/cross/bin/$TARGET-ld --version
